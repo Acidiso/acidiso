0d11b813ba7a9a75968412e2f7e1e567e19b1ca0
Acidiso <32755012+Acidiso@users.noreply.github.com>
2018-12-24 15:19:13 -0200

    Create acidiso.py

---
+++ acidiso.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+#coding: UTF-8
+
+import os
+from Tkinter import *
+import Tkinter, Tkconstants, tkFileDialog
+
+usr = os.getlogin()
+
+root = Tkinter.Tk()
+root.title("AcidISO")
+root.minsize(200,200)
+root.maxsize(200,200)
+root.geometry("200x200")
+img = Tkinter.Image("photo", file="logo1.png")
+root.tk.call('wm','iconphoto',root._w,img)
+texto = Label(root, text="1)- Carregue uma iso\n2)- Clique em montar\n3)- Quando não for utilizar\na iso montada clique\nem desmontar\n")
+texto.pack()
+
+def pickfile(): 
+	root.filename = tkFileDialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("iso files","*.iso"),("all files","*.*")))
+
+files = Tkinter.Button(root, text ="Selecionar ISO", command = pickfile)
+
+files.pack()
+
+def mount():
+	dire = '/home/%s/Acidmnt' % usr
+	os.system("mkdir %s" % dire)
+	direiso = root.filename
+	os.system("gksu 'mount -o loop %s %s' " % (direiso,dire))
+	os.system("exit")
+
+mount = Tkinter.Button(root, text ="Montar ISO", command = mount)
+mount.pack()
+
+def umount():
+	os.system("gksu 'umount /home/%s/Acidmnt' " % (usr))
+	os.system("rm -r /home/%s/Acidmnt" % (usr))
+	os.system("exit")
+
+umount = Tkinter.Button(root, text ="Desmontar ISO", command = umount)
+umount.pack()
+
+root.mainloop()
